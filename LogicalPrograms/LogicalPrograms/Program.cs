8f142a971339bf220691134264904f0d346cce5c
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-20 12:12:06 +0530

    added perfect number

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReverseNumber.PrintTheReverseNumber();
+
+            //FibonacciSeries.PrintingFibonacci();
+
+            PerfectNumber.FindingPerfectNumber();
+
+        }
+    }
+}
